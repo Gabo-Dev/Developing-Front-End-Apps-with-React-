63a6f577c7b1dd3587691880262ef7821fa8a435
Gabo-Dev <52703336+Gabo-Dev@users.noreply.github.com>
2024-07-17 13:45:56 +0200

    event planner added

---
+++ eventPlannerApp/src/Components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const  Footer = () => {
+    return(
+        <footer>
+            <p>&copy; 2024 Jonathan Orna Ponce. All rights reserved.</p>
+        </footer>
+    );
+};
+export default Footer;
